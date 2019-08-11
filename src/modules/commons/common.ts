7f3f735225e54dd5b53e1bc86e1175a5b1c9cd6f
HuyAms <dinhhuyams@gmail.com>
2019-08-11 08:13:32 +0300

    feat: add ServerResponse and ErrorResponse interface in common.ts

--- src/modules/commons/common.ts
+++ src/modules/commons/common.ts
@@ -1,7 +1,18 @@
 import ModelState from '../../models/bases/ModelState'
 
-export const startFetching = <T>(state: ModelState<T>) => {
-	state.status = 'fetching'
+interface ServerResponse<T> {
+	data: T
+	status: number
+}
+
+interface ErrorResponse {
+	errorCode: number
+	message: string
+	status: number
+}
+
+export const startLoading = <T>(state: ModelState<T>) => {
+	state.status = 'loading'
 	state.error = null
 	state.data = null
 }
@@ -11,16 +22,24 @@ export const startSaving = <T>(state: ModelState<T>) => {
 	state.error = null
 }
 
-export const endWithError = <T>(state: ModelState<T>, error: string) => {
+export const endWithError = <T>(
+	state: ModelState<T>,
+	errorResponse: ErrorResponse,
+) => {
 	state.status = 'error'
-	state.error = error
+	state.error = errorResponse.message
 }
 
 export const endCanceling = <T>(state: ModelState<T>) => {
 	state.status = 'idle'
 }
 
-export const updateData = <T>(state: ModelState<T>, data: T) => {
+export const updateData = <T>(
+	state: ModelState<T>,
+	serverResponse: ServerResponse<T>,
+) => {
+	const {data} = serverResponse
+
 	state.status = 'success'
 	state.error = null
 	state.data = data
