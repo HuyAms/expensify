0d012762780511733aaed39521ab1f3aada5e924
HuyAms <dinhhuyams@gmail.com>
2019-08-16 20:50:25 +0300

    refactor: remove lazy import home and board page

--- src/router/AuthenticatedRouter.tsx
+++ src/router/AuthenticatedRouter.tsx
@@ -2,10 +2,9 @@ import * as React from 'react'
 import {Redirect, Route, Switch} from 'react-router-dom'
 import AuthenticatedLayout from '../layout/AuthenticatedLayout'
 import {AuthenticatedRoutePath} from '../models/Route'
+import Home from '../pages/Home'
+import Board from '../pages/Board'
 
-const Home = React.lazy(() =>
-	import(/* webpackChunkName: "Home" */ '../pages/Home'),
-)
 const Report = React.lazy(() =>
 	import(/* webpackChunkName: "Report" */ '../pages/Report'),
 )
@@ -18,6 +17,7 @@ const Router = () => {
 		<AuthenticatedLayout>
 			<Switch>
 				<Route exact path={AuthenticatedRoutePath.home} component={Home} />
+				<Route exact path={AuthenticatedRoutePath.board} component={Board} />
 				<Route path={AuthenticatedRoutePath.report} component={Report} />
 				<Route path={AuthenticatedRoutePath.logout} component={LogOut} />
 				<Redirect to={AuthenticatedRoutePath.home} />
