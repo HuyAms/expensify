73166e56d8ee66019f1bc055ae7d969b299d155e
HuyAms <dinhhuyams@gmail.com>
2019-08-21 15:26:00 +0300

    feat: split AuthenticatedRouter to TeamLayout and NonTeamLayout

--- src/router/AuthenticatedRouter.tsx
+++ src/router/AuthenticatedRouter.tsx
@@ -1,27 +1,17 @@
 import * as React from 'react'
 import {Redirect, Route, Switch} from 'react-router-dom'
 import AuthenticatedLayout from '../layout/AuthenticatedLayout'
+import TeamLayout from '../layout/TeamLayout'
+import NonTeamLayout from '../layout/NonTeamLayout'
 import {AuthenticatedRoutePath} from '../models/Route'
-import Home from '../pages/Home'
-import Board from '../pages/Board'
-
-const Report = React.lazy(() =>
-	import(/* webpackChunkName: "Report" */ '../pages/Report'),
-)
-const LogOut = React.lazy(() =>
-	import(/* webpackChunkName: "LogOut" */ '../pages/Auth/LogOut'),
-)
+import LogOut from '../pages/Auth/LogOut'
 
 const Router = () => {
 	return (
 		<AuthenticatedLayout>
 			<Switch>
-				<Route exact path={AuthenticatedRoutePath.home} component={Home} />
-				<Route
-					path={`${AuthenticatedRoutePath.board}/:slug`}
-					component={Board}
-				/>
-				<Route path={AuthenticatedRoutePath.report} component={Report} />
+				<Route path="/team/:slug" component={TeamLayout} />
+				<Route path="/" component={NonTeamLayout} />
 				<Route path={AuthenticatedRoutePath.logout} component={LogOut} />
 				<Redirect to={AuthenticatedRoutePath.home} />
 			</Switch>
