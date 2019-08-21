00f8bb235a37fe43e5031bf5e9798c4869abbc6f
HuyAms <dinhhuyams@gmail.com>
2019-08-21 18:01:09 +0300

    style: add style for not found page

---
+++ src/pages/TeamNotFound/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 90%;
+	margin: 0 auto;
+`
+
+export const ErrorImg = styled.img`
+	max-width: 30rem;
+`
+
+export const ErrorText = styled.h2`
+	text-align: center;
+	margin-bottom: 3rem;
+`
