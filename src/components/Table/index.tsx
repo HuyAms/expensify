22d9084878c32c6cee952fee384c39bf4debd1cb
HuyAms <dinhhuyams@gmail.com>
2019-09-25 08:42:25 +0300

    refactor: remove any from renderEdittingCell arg

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -16,7 +16,7 @@ interface Column {
 	dataIndex: string
 	editable?: boolean
 	required?: boolean
-	renderEditingCell?: (arg: any) => any
+	renderEditingCell?: (arg) => any
 }
 
 interface Props<T> {
