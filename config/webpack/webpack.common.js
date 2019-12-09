d55faad5cfc820707e72c6f026538d48e43f87e2
HuyAms <dinhhuyams@gmail.com>
2019-12-09 15:50:32 +0200

    chore: add missing webpack import

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
